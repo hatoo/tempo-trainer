8013991a460f88c4a5f74bb3e04467c172c2749b
hatoo <hato2000@gmail.com>
2025-02-23 15:53:24 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::time::{Duration, Instant};
+use std::{
+    collections::VecDeque,
+    time::{Duration, Instant},
+};
 
 use bevy::{
     prelude::*,
@@ -9,6 +12,7 @@ const CLICK_AUDIO_PATH: &str = "sounds/c5.ogg";
 const TAP_AUDIO_PATH: &str = "sounds/c4.ogg";
 
 const CIRCLE_SIZE: f32 = 400.0;
+const BINS: usize = 16;
 
 #[derive(Component)]
 struct StatusText;
@@ -22,15 +26,19 @@ struct LastTick(Instant);
 #[derive(Resource)]
 struct Division(u32);
 
+#[derive(Resource)]
+struct TapDeltas(VecDeque<f64>);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Time::<Fixed>::from_duration(from_bpm(90.0)))
         .insert_resource(LastTick(Instant::now()))
         .insert_resource(Division(1))
+        .insert_resource(TapDeltas(VecDeque::new()))
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, metronome)
-        .add_systems(Update, (tap, control, clock, set_status_text))
+        .add_systems(Update, (tap, control, clock, set_status_text, set_bins))
         .run();
 }
 
@@ -93,6 +101,11 @@ fn setup(
             ..Default::default()
         },
     ));
+
+    commands.spawn(BarChart::new(&mut meshes, &mut materials));
+    for i in 0..BINS {
+        commands.spawn(Bin::new(i, &mut meshes, &mut materials));
+    }
 }
 
 fn tap(
@@ -102,6 +115,7 @@ fn tap(
     last_tick: Res<LastTick>,
     timer: Res<Time<Fixed>>,
     division: Res<Division>,
+    mut tap_deltas: ResMut<TapDeltas>,
 ) {
     if keyboard_input.get_just_pressed().count() > 0 {
         commands.spawn(AudioPlayer::new(asset_server.load(TAP_AUDIO_PATH)));
@@ -116,13 +130,17 @@ fn tap(
         let delta_last = (now - last_tick).as_secs_f64() % time_step_div.as_secs_f64();
         let delta_next = (next_tick - now).as_secs_f64() % time_step_div.as_secs_f64();
 
-        let delta_ms = if delta_last < delta_next {
-            delta_last * 1000.0
+        let delta = if delta_last < delta_next {
+            delta_last
         } else {
-            -(delta_next * 1000.0)
+            -delta_next
         };
 
-        dbg!(delta_ms);
+        tap_deltas.0.push_front(delta);
+        while tap_deltas.0.len() > BINS {
+            tap_deltas.0.pop_back();
+        }
+        dbg!(&tap_deltas.0);
     }
 }
 
@@ -205,4 +223,71 @@ fn clock(
 
     let mut transform = query.single_mut();
     transform.translation = Vec3::new(angle.sin() * CIRCLE_SIZE, angle.cos() * CIRCLE_SIZE, 1.0);
+}
+
+#[derive(Bundle)]
+struct BarChart {
+    line: (Mesh2d, MeshMaterial2d<ColorMaterial>, Transform),
+}
+
+impl BarChart {
+    fn new(
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+    ) -> Self {
+        Self {
+            line: (
+                Mesh2d(meshes.add(Mesh::from(Rectangle {
+                    half_size: Vec2::new(1000.0, 1.0),
+                }))),
+                MeshMaterial2d(materials.add(Color::BLACK)),
+                Transform::from_xyz(0.0, 0.0, 2.0),
+            ),
+        }
+    }
+}
+
+#[derive(Component)]
+struct BinIndex(usize);
+
+#[derive(Bundle)]
+struct Bin {
+    index: BinIndex,
+    rect: Mesh2d,
+    material: MeshMaterial2d<ColorMaterial>,
+    transform: Transform,
+}
+
+impl Bin {
+    fn new(
+        index: usize,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+    ) -> Self {
+        let mut transform = Transform::from_xyz(0.0, 0.0, 4.0);
+        transform.scale = Vec3::ZERO;
+        transform.translation.x = (index as f32 - (BINS / 2) as f32) * 100.0;
+        Self {
+            index: BinIndex(index),
+            rect: Mesh2d(meshes.add(Mesh::from(Rectangle {
+                half_size: Vec2::new(0.5, 0.5),
+            }))),
+            material: MeshMaterial2d(materials.add(Color::linear_rgb(0.0, 0.0, 1.0))),
+            transform,
+        }
+    }
+}
+
+fn set_bins(mut query: Query<(&BinIndex, &mut Transform)>, tap_deltas: Res<TapDeltas>) {
+    if tap_deltas.is_changed() {
+        for (BinIndex(index), mut transform) in &mut query {
+            if let Some(delta) = tap_deltas.0.get(*index) {
+                let height = *delta as f32 * 1000.0;
+                transform.translation.y = height / 2.0;
+                transform.scale = Vec3::new(88.0, height, 1.0);
+            } else {
+                transform.scale = Vec3::ZERO;
+            }
+        }
+    }
 }
