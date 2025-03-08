a26e58fe446b8c2e47766f4d99d56f8d99d16979
hatoo <hato2000@gmail.com>
2025-03-08 14:16:56 +0900

    wip

---
+++ mobile/src/lib.rs
@@ -0,0 +1,22 @@
+use bevy::prelude::*;
+use bevy::window::WindowMode;
+use bevy::winit::WinitSettings;
+use tempo_trainer::GamePlugin;
+
+#[bevy_main]
+fn main() {
+    App::new()
+        .insert_resource(WinitSettings::mobile())
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resizable: false,
+                    mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+                    ..default()
+                }),
+                ..default()
+            }),
+            GamePlugin,
+        ))
+        .run();
+}
